8f5af6b7fcc4af461f7af689780560662e74b9d0
Viktor Grandgeorg <info@intelligibel.de>
2017-10-16 10:20:06 +0200

    Add cron.php for php-cli cronjob.

---
+++ cron.php
@@ -0,0 +1,14 @@
+<?php
+$threshold = 1 * 24 * 60 *60; // a day
+
+$baseDir = dirname(__FILE__);
+$downloadsDir = $baseDir . DIRECTORY_SEPARATOR . 'public'. DIRECTORY_SEPARATOR . 'downloads';
+$files = scandir($downloadsDir);
+$maxAge = time() - $threshold;
+foreach ($files as $file) {
+    $filePath = $downloadsDir . DIRECTORY_SEPARATOR . $file;
+    if (is_file($filePath) && filemtime($filePath) < $maxAge) {
+        unlink($filePath);
+    }
+}
+?>
